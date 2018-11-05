0deddcc3047c50876d6198b8a59e0ca17e27c828
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-11-05 11:28:28 -0500

    cleaned up code

--- src/services/api_acess.js
+++ src/services/api_acess.js
@@ -1,41 +1,41 @@
 const api_root = "http://localhost:3000/game";
-let playerId = 0;
+export let playerId = null;
 
 export function GetState(){
-    return myFetch(api_root + "/");  
+    return myFetch(api_root + "/");
 }
-
 export function GetMyCaptions(){
-    return myFetch(api_root + `/players`, {name: "Moshe"})
-     .then(x=> myFetch(api_root + `/captions/${playerId}`));
+    return myFetch(api_root + `/captions/${playerId}`);
 }
-
-
+export function Login(name){
+    return myFetch(api_root + `/players`, { name: name })
+            .then(x=> playerId = x.id);
+}
 export function FlipPicture(){
-    return myFetch(api_root + "/picture", {})      
+    return myFetch(api_root + "/picture", {})
 }
 
 
-function myFetch(url = ``, data = null) {
-    let options = {
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, same-origin, *omit
-    };
-    
-    if(data){
-        options = { 
-             ...options,
-             method: "POST", // *GET, POST, PUT, DELETE, etc.
-             headers: {
-                 "Content-Type": "application/json; charset=utf-8",
+
+
+  function myFetch(url = ``, data = null) {
+      let options = {
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+      };
+      if(data){
+          options = { 
+            ...options,
+            method:  "POST", // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
                 // "Content-Type": "application/x-www-form-urlencoded",
-            }, 
+            },
             body: JSON.stringify(data), // body data type must match "Content-Type" header
-        };
-
-    }
-    return fetch(url, options)
-      .then(response => {
-          return response.json()
-        }); // parses response to JSON
-}
+          };
+      }
+      return fetch(url, options)
+      .then(response =>{
+        return response.json()
+      }); // parses response to JSON
+  }
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,22 +3,29 @@
     <div class="alert alert-success">
         Yay we have a game!
     </div>
+
     <div class="row">
         <div class="col-md-4">
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Players</h5>
-                    <li v-for="p in myCaptions" class="list-group-item">{{p.name}}</li>
-
-                </div>
+                    <h5 class="card-header">
+                        Players
+                        <a @click.prevent="login" class="btn btn-sm btn-primary" :class="{disabled: playerId !== null}">+</a>
+                    </h5>
+                    <ul class="list-group list-group-flush">
+                        <li v-for="p in state.players" :key="p.id"
+                            class="list-group-item">
+                            <img />
+                            <h5>{{p.name}}</h5>
+                            <span class="badge badge-primary badge-pill">{{p.score}}</span>
+                        </li>
+ 
+                    </ul>
             </div>
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">My Captions</h5>
-                    <ul class="list-group list-group-flush">
-                        <li v-for="c in myCaptions" class="list-group-item">{{c}}</li>
-                    </ul>
-                  </div>
+                <h5 class="card-header">My Captions</h5>
+                <ul class="list-group list-group-flush">
+                    <li v-for="c in myCaptions" :key="c" class="list-group-item">{{c}}</li>
+                </ul>
             </div>
         </div>
         <div class="col-md-4">
@@ -30,51 +37,66 @@
         </div>
         <div class="col-md-4">
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Played Captions</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                <h5 class="card-header">Played Captions</h5>
+                <ul class="list-group list-group-flush">
+                    <li v-for="c in state.playedCaptions" :key="c.text" class="list-group-item">{{c}}</li>
+                </ul>
             </div>
         </div>
     </div>
- </div>   
+</div>
 </template>
 
 <style lang="scss">
-
+    li.list-group-item {
+        display: flex;
+        align-content: center;
+        justify-content: space-between;
+        img {
+            width: 30px; height: 30px;
+            margin-right: 5px;
+        }
+        h5 {
+            flex-grow: 1;
+        }
+    }
 </style>
 
-
 <script>
-import { GetState, FlipPicture, GetMyCaptions } from '@/services/api_acess'
+import * as api from '@/services/api_acess';
 export default {
-    data: function(){
+    data(){
         return {
             state: {
                 picture: "",
                 players: [],
-                playedCaptions : [],
+                playedCaptions: [],
             },
-            myCaptions : [],
-
-
+            myCaptions: [],
+            //playerId: null
         }
     },
-    created: function(){
-        GetState()
-        .then(x => this.state = x);
-        GetMyCaptions()
-        .then(x=> this.myCaptions = x);
+    created(){
+        this.refresh();
     },
     methods: {
-        flipPicture: function(){
-            FlipPicture()
-            .then(x => GetState())
-            .then(x => this.state = x)
+        refresh(){
+            api.GetState()
+            .then(x=> this.state = x)
+        },
+        flipPicture(){
+            api.FlipPicture()
+            .then(()=> this.refresh())
+        },
+        login() {
+            api.Login(prompt('What is your name?'))
+            .then(()=>  api.GetMyCaptions().then(x=> this.myCaptions = x) )
+            .then(()=> this.refresh())
         }
+        
+    },
+    computed: {
+        playerId: ()=> api.playerId
     }
-
-    
 }
 </script>
